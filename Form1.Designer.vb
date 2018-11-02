1fff610b3d8d0bc98c95761d2cd2221f362ea1ed
CUngerJr <chrisunger2010@gmail.com>
2018-11-02 14:31:22 -0400

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblEmps = New System.Windows.Forms.Label()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(128, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Total Income:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(70, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(171, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Number of Employees:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(107, 211)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(129, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Average Income:"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotal.Location = New System.Drawing.Point(257, 51)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(181, 23)
+        Me.lblTotal.TabIndex = 3
+        Me.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblEmps
+        '
+        Me.lblEmps.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblEmps.Location = New System.Drawing.Point(292, 120)
+        Me.lblEmps.Name = "lblEmps"
+        Me.lblEmps.Size = New System.Drawing.Size(93, 23)
+        Me.lblEmps.TabIndex = 4
+        Me.lblEmps.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblAverage
+        '
+        Me.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAverage.Location = New System.Drawing.Point(257, 210)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(181, 23)
+        Me.lblAverage.TabIndex = 5
+        Me.lblAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(490, 385)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(83, 17)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Chris Unger"
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(73, 307)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(106, 36)
+        Me.btnEnter.TabIndex = 7
+        Me.btnEnter.Text = "&Enter Income"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(241, 308)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(106, 36)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(413, 308)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(106, 35)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(585, 416)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.lblEmps)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Compute Average Income"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+    Friend WithEvents lblEmps As System.Windows.Forms.Label
+    Friend WithEvents lblAverage As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents btnEnter As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,61 @@
+﻿'Chris Unger
+'CSC 101-02
+'Assignment 8 - Average Income of Employees
+'Purpose - Using Input Box, Do While Loop, and Counters to compute total and average income of Employees
+
+Public Class Form1
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        'Declare Variables
+        Const intNUM_EMP As Integer = 5         'The number of Employees
+        Dim strInput As String                  'Hold string Input
+        Dim decAverage As Decimal
+        Dim decIncome As Decimal                'Hold Income
+        Dim intCount As Integer = 1             'Loop Counter
+        Dim decTotal As Decimal = 0             'To Hold Total Income
+
+        ' Get the Number of Employees
+        Do While intCount <= intNUM_EMP
+
+            ' Get the Pay of the Employees
+            strInput = InputBox("Enter the pay of the Employee" &
+                       intCount.ToString(), "Input Income")
+
+            ' Convert the Input to Decimal
+            If Decimal.TryParse(strInput, decIncome) Then
+
+                ' Add the Employee Pay to the Total Income
+                decTotal += decIncome
+
+                ' Add 1 to the Loop Counter
+                intCount += 1
+            Else
+                ' Display an Error Message for Invalid Data
+                MessageBox.Show("Enter a Numeric Value.")
+            End If
+        Loop
+
+        'Compute the Average
+        decAverage = decTotal / intNUM_EMP
+
+        ' Display the Total Sales
+        lblTotal.Text = decTotal.ToString("c")
+        lblEmps.Text = intNUM_EMP.ToString("n0")
+        lblAverage.Text = decAverage.ToString("c")
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear All Labels
+        lblEmps.Text = String.Empty
+        lblAverage.Text = String.Empty
+        lblTotal.Text = String.Empty
+
+        'Focus returns on btnEnter
+        btnEnter.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Exits Application
+        Me.Close()
+    End Sub
+End Class
